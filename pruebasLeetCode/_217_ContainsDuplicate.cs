167fa0def0b6486f3312813d93393d291a17ff01
Ezkizo <luisfuentes@villaguerrero.tecnm.mx>
2025-02-26 21:26:36 -0600

    Resuelto ContainsDuplicate

---
+++ pruebasLeetCode/_217_ContainsDuplicate.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace pruebasLeetCode;
+public class _217_ContainsDuplicate
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        //Mi solución
+        var hash = new HashSet<int>(nums);
+        return (hash.Count != nums.Length);
+    }
+}
