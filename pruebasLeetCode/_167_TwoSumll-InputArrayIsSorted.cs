d0f4d041f5be9d68429958ce42c675a1a270110d
Ezkizo <luisfuentes@villaguerrero.tecnm.mx>
2025-02-28 14:21:50 -0600

    Solución TwoSum ll

---
+++ pruebasLeetCode/_167_TwoSumll-InputArrayIsSorted.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace pruebasLeetCode;
+class _167_TwoSumll_InputArrayIsSorted
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var numLen = numbers.Length;
+        int[] res = new int[2];
+        int left = 0, right = numLen - 1;
+
+        while (left < right)
+        {
+            var sum = numbers[left] + numbers[right];
+            if (sum == target)
+            {
+                res[0] = left + 1;
+                res[1] = right + 1;
+            }
+            if (sum > target)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return res;
+    }
+}
