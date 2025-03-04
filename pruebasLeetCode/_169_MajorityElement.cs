c817d77356d57b135bec772cb19248ff7860354f
Ezkizo <luisfuentes@villaguerrero.tecnm.mx>
2025-03-03 23:01:09 -0600

    Solución MajorityElement

---
+++ pruebasLeetCode/_169_MajorityElement.cs
@@ -0,0 +1,28 @@
+﻿namespace pruebasLeetCode;
+public class _169_MajorityElement
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> cont = new();
+        int res = 0, max = int.MinValue;
+
+        foreach (var num in nums)
+        {
+            if (cont.ContainsKey(num))
+            {
+                cont[num]++;
+                continue;
+            }
+            cont.Add(num, 1);
+        }
+        foreach (var num in cont.Keys)
+        {
+            if (cont[num] > max)
+            {
+                max = cont[num];
+                res = num;
+            }
+        }
+        return res;
+    }
+}
